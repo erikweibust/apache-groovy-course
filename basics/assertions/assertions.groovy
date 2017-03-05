58fb86e7fcdcb0093aaff6522a052af7698a1869
Erik Weibust <erik@weibust.net>
2017-03-05 09:29:14 -0600

    resolved failing assertions in script

--- basics/assertions/assertions.groovy
+++ basics/assertions/assertions.groovy
@@ -10,15 +10,18 @@ def x = 1
 assert x == 1
 
 // what happens when the expression doesn't evaluate to true? 
-assert false
+//assert false
 
 // The power assertion output shows evaluation results from the outer to the inner expression
-assert 1 == 2
+//assert 1 == 2
+assert 1 == 1
 
 // complex debug output
-assert 1 == (3 + 10) * 100 / 5 * 20
+//assert 1 == (3 + 10) * 100 / 5 * 20
+assert 5200 == (3 + 10) * 100 / 5 * 20 // correcting the above line
 
 // The power assertion statements true power unleashes in complex Boolean statements, 
 // or statements with collections or other toString-enabled classes:
-def x = [1,2,3,4,5]
-assert (x << 6) == [6,7,8,9,10]
+def x2 = [1,2,3,4,5]
+//assert (x2 << 6) == [6,7,8,9,10]
+assert (x2 << 6) == [1,2,3,4,5,6]
